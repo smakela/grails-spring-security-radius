ac848c7f737e562d952986cdcf2d6a52766a3c7e
Sami Mäkelä <smakela@iki.fi>
2011-11-25 08:02:29 +0200

    cleaning up

--- src/groovy/org/codehaus/groovy/grails/plugins/springsecurity/radius/GrailsRadiusAuthenticatorImpl.groovy
+++ src/groovy/org/codehaus/groovy/grails/plugins/springsecurity/radius/GrailsRadiusAuthenticatorImpl.groovy
@@ -1,17 +1,17 @@
 /* Copyright 2011 the original author or authors.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.codehaus.groovy.grails.plugins.springsecurity.radius
 
 import net.jradius.client.RadiusClient
@@ -37,11 +37,13 @@ import org.springframework.util.Assert
 import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
 
 /**
-* @author <a href="mailto:smakela@iki.fi">Sami Mäkelä</a>
-*/
+ * @author <a href="mailto:smakela@iki.fi">Sami Mäkelä</a>
+ */
 class GrailsRadiusAuthenticatorImpl implements GrailsRadiusAuthenticator, InitializingBean {
 
-    static final List NO_ROLES = [new GrantedAuthorityImpl(SpringSecurityUtils.NO_ROLE)]
+    static final List NO_ROLES = [
+        new GrantedAuthorityImpl(SpringSecurityUtils.NO_ROLE)
+    ]
 
     RadiusAuthenticator radiusAuthenticator
     String radiusHost
@@ -57,7 +59,7 @@ class GrailsRadiusAuthenticatorImpl implements GrailsRadiusAuthenticator, Initia
 
     @Override
     public UserDetails authenticate(
-            UsernamePasswordAuthenticationToken authentication) {
+    UsernamePasswordAuthenticationToken authentication) {
 
         String username = authentication.getPrincipal()
         String password = authentication.getCredentials()
@@ -69,7 +71,8 @@ class GrailsRadiusAuthenticatorImpl implements GrailsRadiusAuthenticator, Initia
         RadiusPacket reply
 
         try {
-            RadiusClient radiusClient = new RadiusClient(InetAddress.byName(radiusHost), sharedSecret, authenticationPort, accountingPort, timeout)
+            RadiusClient radiusClient = new RadiusClient(InetAddress.byName(radiusHost),
+                sharedSecret, authenticationPort, accountingPort, timeout)
             RadiusPacket request = new AccessRequest(radiusClient, radiusAttributes)
             radiusClient.authenticate(request, radiusAuthenticator, retries)
         } catch (RadiusException re) {
@@ -85,7 +88,7 @@ class GrailsRadiusAuthenticatorImpl implements GrailsRadiusAuthenticator, Initia
         }
 
         if (!(reply instanceof AccessAccept)) {
-           throw new BadCredentialsException("Bad credentials")
+            throw new BadCredentialsException("Bad credentials")
         }
 
         new User(username, password, true, true, true, true, NO_ROLES);
